2f621cc1a97b51428344846efea254161258b359
peymanelfc <88836880+peymanelfc@users.noreply.github.com>
2021-10-04 10:56:20 +0330

    dsf
    dfs

--- controller/soft/back/RegisterUser.js
+++ controller/soft/back/RegisterUser.js
@@ -9,4 +9,17 @@ module.exports = new class RegisterUser {
     {
         return res.json('test1');
     }
+    generate_code (code) {
+    if (code == null) {
+        return "not_valid";
+    }
+    let count = 0;
+    let code_2 = code.trim().toUpperCase();
+    for (let i = 0; i < code_2.length; i++) {
+        count += check_number(code_2[i]);
+    }
+
+    return Buffer.from(count.toString()).toString("base64");
+
+    }
 }
